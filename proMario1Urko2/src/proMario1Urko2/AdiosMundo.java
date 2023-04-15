2f344385d6abea22ce5d86946a7e0665f69e342e
mario <mario@Mario26>
2023-04-15 13:11:33 +0200

    version2

--- proMario1Urko2/src/proMario1Urko2/AdiosMundo.java
+++ proMario1Urko2/src/proMario1Urko2/AdiosMundo.java
@@ -5,6 +5,7 @@ public class AdiosMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hola Mundo");
+System.out.println("Adios Mundo");
 	}
 
 }
